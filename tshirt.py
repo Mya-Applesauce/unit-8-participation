f5ce1b9b7fe1fbc9343d339fea77bdc0a4b800e1
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-08 00:47:18 -0500

    added keyword arguements

--- tshirt.py
+++ tshirt.py
@@ -1,4 +1,5 @@
 def make_shirt(size, message):
     print(f"We shalt print thy size {size} shirt, inscribed with the message '{message}', forthwith.")
 
-make_shirt("small", "rouxlsrules")
+make_shirt("medium", "rouxlsrules")
+make_shirt(message="It's TV Time!", size="large")
