9ac8a5cfb223a85fcae3bcaabccd91f380b19cfa
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-08 11:05:00 -0400

    initial commit

---
+++ 8-9_messages.py
@@ -0,0 +1,7 @@
+messages = ["A world basked in purest light. Beneath it, grew eternal night.", "If foutains freed, the roaring cries. And titans shape from darkened eyes.", "The light and dark both burning dire. A countdown to the earth's expire.", "But lo, on hopes and dreams they send. Three Heroes at the world's end."]
+
+def show_messages():
+    for message in messages:
+        print(message)
+
+show_messages()
