1a865fe5fb6c312ecf676565cd84f055bf0878e2
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-08 00:54:27 -0500

    added default values

--- tshirt.py
+++ tshirt.py
@@ -1,5 +1,8 @@
-def make_shirt(size, message):
+def make_shirt(size="large", message="I <3 python!"):
     print(f"We shalt print thy size {size} shirt, inscribed with the message '{message}', forthwith.")
 
 make_shirt("medium", "rouxlsrules")
 make_shirt(message="It's TV Time!", size="large")
+make_shirt()
+make_shirt(size="medium")
+make_shirt(message="Sweet Cap'n Cakes: Cyber Battle (Solo)")
