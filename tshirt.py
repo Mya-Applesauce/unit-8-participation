89dd5657b71172083c394913e067f4a98cc51f6f
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-07 23:15:14 -0500

    actual initial commit

---
+++ tshirt.py
@@ -0,0 +1,6 @@
+def make_shirt():
+    size = int(input("What size of garment doth thou requirest?"))
+    message = input("And what is the message thou wisheth to have printed on it?")
+    print(f"We shalt print thy size {size} shirt, inscribed with the message '{message}', forthwith.")
+
+make_shirt()
